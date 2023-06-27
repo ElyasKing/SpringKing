c4b3e8dc6abd7d23ba914599a05acc2ac17340f2
Elyas <123657387+ElyasKing@users.noreply.github.com>
2023-06-27 16:53:57 +0200

    Version Good 1

--- banque/src/test/java/com/example/banque/BanqueApplicationTests.java
+++ banque/src/test/java/com/example/banque/BanqueApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.tdspring;
+package com.example.banque;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TdSpringApplicationTests {
+class BanqueApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
